48d846565ae9a25fb0fd6d51c754477a807a0a84
skutner <sava.dumitru.daniel@gmail.com>
2020-09-25 10:35:41 +0300

    cleanup

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ const GtinDSUFactory = require("./GTIN_DSU_Factory");
 
 resolver.registerDSUFactory("gtin", new GtinDSUFactory(resolver));
 module.exports = require("./GTIN_SSI");
-
-const gtinResolver = require("./gtin-resolver");
-gtinResolver.createGTIN_SSI()
